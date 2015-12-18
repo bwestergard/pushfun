20c24ab87c2d237b95e4612b8a860242ae80070b
Björn Westergard <bjorn.westergard@lanetix.com>
2015-12-18 13:35:35 -0500

    semicolons
    Signed-off-by: Björn Westergard <bjorn.westergard@lanetix.com>

--- foo.js
+++ foo.js
@@ -4,14 +4,14 @@ exports.handler = function (event, context) {
       to: 'management@acme.com',
       subject: 'Duck depot capacity expanded!',
       body: 'The duck depot can now handle' + event.changeSet.capacity
-    }])
+    }]);
   } else if ((event.record.capacity > 10) && (event.changeSet.capacity < 10)) {
     context.succeed([{
       to: 'operations@acme.com',
       subject: 'Duck depot capacity critically low!',
       body: 'The duck depot can now handle' + event.changeSet.capacity
-    }])
+    }]);
   } else {
-    return []
+    return [];
   }
 }
