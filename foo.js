738c55b6aa0987dac53d26c15ed626c11832d155
Björn Westergard <bjorn.westergard@lanetix.com>
2015-12-18 13:25:27 -0500

    more interesting demo
    Signed-off-by: Björn Westergard <bjorn.westergard@lanetix.com>

--- foo.js
+++ foo.js
@@ -1,5 +1,17 @@
 exports.handler = function(event, context) {
-  context.succeed(
-    (event.record.capacity < 30) && (event.changeSet.capacity > 30)
-  )
+  if ((event.record.capacity < 30) && (event.changeSet.capacity > 30)) {
+    context.succeed([{
+      to: 'management@acme.com',
+      subject: `Duck depot capacity expanded!`,
+      body: 'The duck depot can now handle' + event.changeSet.capacity
+    }])
+  } else if ((event.record.capacity > 10) && (event.changeSet.capacity < 10)) {
+    context.succeed([{
+      to: 'management@acme.com',
+      subject: `Duck depot capacity critically low!`,
+      body: 'The duck depot can now handle' + event.changeSet.capacity
+    }])
+  } else {
+    return []
+  }
 }
