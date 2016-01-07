6a592020468f1ed73cd00552e2c473592a7da92b
Björn Westergard <bjorn.westergard@lanetix.com>
2016-01-07 16:48:10 -0500

    fix if blocks
    Signed-off-by: Björn Westergard <bjorn.westergard@lanetix.com>

--- foo.js
+++ foo.js
@@ -2,14 +2,13 @@ exports.handler = function (event, context) {
   var message;
   if(!event.changeSet.trash_cans) {
     message = "no operation";
-  };
-
-  if (event.changeSet.trash_cans === 12731122) {
-    message = "Yay!"
   } else {
-    message = "Boo!"
+    if (event.changeSet.trash_cans === 12731122) {
+      message = "Yay!"
+    } else {
+      message = "Boo!"
+    }
   }
-
   context.succeed({
     'message': message,
     'changes': event.changeSet
