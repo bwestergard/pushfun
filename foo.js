42709ed173e0535e83991af7e748c00daa932e3d
Björn Westergard <bjorn.westergard@lanetix.com>
2016-01-07 16:11:23 -0500

    improved mother earth
    Signed-off-by: Björn Westergard <bjorn.westergard@lanetix.com>

--- foo.js
+++ foo.js
@@ -1,11 +1,13 @@
 exports.handler = function (event, context) {
-  if(event.trash_cans == '12731122') {
+  if(event.trash_cans === '12731122') {
     context.succeed({
-      'message': 'thanks for recycling!'
+      'message': 'thanks for recycling!',
+      'option': event.trash_cans
     });
   } else {
     context.succeed({
-      'message': 'You have disappointed mother earth.'
+      'message': 'You have disappointed mother earth.',
+      'option': event.trash_cans
     });
   }
 }
